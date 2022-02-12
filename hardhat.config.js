854633aed9a3e53736c72a1ac650d3e342684804
dimkin7 <dima-besh@yandex.ru>
2022-02-12 22:23:53 +0300

    Создание hardhat task

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,59 +3,45 @@ require("solidity-coverage")
 require("dotenv").config();
 require("@nomiclabs/hardhat-web3");
 
+//https://rinkeby.etherscan.io/address/0x8bbc205ec38f6aca8da9e82cbfe76662a5e0b987
 
+//npx hardhat makeDonation --network rinkeby --key PRIVATE_KEY --amount 333
 task("makeDonation", "Make donation")
   .addParam("key", "Your private key")
   .addParam("amount", "Amount")
   .setAction(async (taskArgs) => {
 
     const provider = new ethers.providers.AlchemyProvider(network = "rinkeby");
-
+    const contractAddr = '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987';
     const sender = new ethers.Wallet(taskArgs.key, provider);
 
-    const contract = '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987';
-
     const tx2 = await sender.sendTransaction({
-      to: contract,
+      to: contractAddr,
       value: taskArgs.amount,
     });
     await tx2.wait();
   });
 
-/*
-task("makeDonation", "Make donation", async (taskArgs, hre) => {
-  //Donation.makeDonation();
+//npx hardhat withdraw --network rinkeby --key PRIVATE_KEY --toaddr 0x7EA751f8B46E08F7397904A39b3e08901B5D1659 --amount 22
+task("withdraw", "Withdraw")
+  .addParam("key", "Your private key")
+  .addParam("toaddr", "To address")
+  .addParam("amount", "Amount")
+  .setAction(async (taskArgs) => {
 
-  /*const accounts = await hre.ethers.getSigners();
+    const abi = [
+      "function withdraw(address _sendTo, uint256 _amount)"
+    ];
+    const provider = new ethers.providers.AlchemyProvider(network = "rinkeby");
+    const contractAddr = '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987';
+    const signer = new ethers.Wallet(taskArgs.key, provider);
+    const donator2022 = new ethers.Contract(contractAddr, abi, signer);
+    
+    //console.log(donator2022);
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-
-
-
-
-
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-    web3.eth.sendTransaction({
-      from: account.address,
-      to: '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987',
-      value: 333
-    })
-      .then(function (receipt) {
-        console.log(receipt);
-      });
-  }
-
-
-
-  
-
-
-});*/
+    withdraw = await donator2022.withdraw(taskArgs.toaddr, taskArgs.amount);
+    await withdraw.wait();
+  });
 
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
