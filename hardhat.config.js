7603e3c5539f82cccf2d06db391497332fabf5dc
dimkin7 <dima-besh@yandex.ru>
2022-02-12 19:52:42 +0300

    Тесты написаны

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,28 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage")
+require("dotenv").config();
+require("@nomiclabs/hardhat-web3");
 
 
+task("makeDonation", "Make donation")
+  .addParam("key", "Your private key")
+  .addParam("amount", "Amount")
+  .setAction(async (taskArgs) => {
 
+    const provider = new ethers.providers.AlchemyProvider(network = "rinkeby");
+
+    const sender = new ethers.Wallet(taskArgs.key, provider);
+
+    const contract = '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987';
+
+    const tx2 = await sender.sendTransaction({
+      to: contract,
+      value: taskArgs.amount,
+    });
+    await tx2.wait();
+  });
+
+/*
 task("makeDonation", "Make donation", async (taskArgs, hre) => {
   //Donation.makeDonation();
 
@@ -10,16 +30,39 @@ task("makeDonation", "Make donation", async (taskArgs, hre) => {
 
   for (const account of accounts) {
     console.log(account.address);
-  }*/
-});
+  }
 
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+
+
+
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
+    web3.eth.sendTransaction({
+      from: account.address,
+      to: '0x8BBc205Ec38F6AcA8dA9e82Cbfe76662A5E0B987',
+      value: 333
+    })
+      .then(function (receipt) {
+        console.log(receipt);
+      });
   }
+
+
+
+  
+
+
+});*/
+
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const [contract] = await ethers.getSigners();
+
+  console.log(contract.address);
+
 });
 
 // You need to export an object to set up your config
@@ -30,4 +73,10 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RENKEBY_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  }
 };
